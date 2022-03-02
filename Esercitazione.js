f3fcd50ccd9d4b37f40c4ed22408df9de42bbd0f
SabrinaTuravani <sturavani@studenti.apuliadigitalmaker.it>
2022-03-02 13:02:06 +0100

    Init reposity

---
+++ Esercitazione.js
@@ -0,0 +1,5 @@
+var studenti = [
+    { nome: "Antonella", cognome: "Bianchi", età: 26},
+    { nome: "Francesca", cognome: "Verdi" , età: 20},
+    { nome: "Lorenza", cognome: "Rossi", età: 21}
+]
---
+++ script.js
@@ -0,0 +1,27 @@
+//Funzione per stampare la data
+function displayDate() {
+    document.getElementById("demo").innerHTML = "<p>Current date and time:" + Date() + "</p>";
+}
+
+/*var s = 4;
+if (s == 5) {
+    console.log(5)
+} else if(s == 4){
+    console.log(4)
+}
+var t=5;
+var par=2;
+
+function a() {
+    var r = par;
+    return s= r+t;
+}
+console.log(a());*/
+
+
+
+/* 
+Commento
+su più 
+righe
+*/
